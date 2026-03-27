f419cc7f789af8966bcb660705c11988753cd1a4
Breno Morais <brenomorais88@gmail.com>
2026-03-27 15:14:32 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.0"
+    application
+}
+
+group = "com.gearsales"
+version = "1.0.0"
+
+application {
+    mainClass.set("com.gearsales.leadengine.ApplicationKt")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-thymeleaf-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm")
+    implementation("io.ktor:ktor-server-html-builder-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+
+    implementation("org.jetbrains.exposed:exposed-core:0.55.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.55.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.55.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.55.0")
+
+    implementation("org.xerial:sqlite-jdbc:3.46.1.3")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+
+    implementation("org.apache.poi:poi-ooxml:5.3.0")
+
+    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
+    implementation("org.thymeleaf:thymeleaf-spring6:3.1.2.RELEASE")
+
+    implementation("ch.qos.logback:logback-classic:1.5.8")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+}
---
+++ src/main/kotlin/com/gearsales/leadengine/Application.kt
@@ -0,0 +1,4 @@
+package com.gearsales.leadengine
+
+class Application {
+}
